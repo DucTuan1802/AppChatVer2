3aa7fae3f84372332b1d9be7f9b3b91f4de27abe
DucTuan1802 <105335784+DucTuan1802@users.noreply.github.com>
2022-05-11 22:43:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var server = require("http").createServer(app);
 var io = require("socket.io")(server);
 var fs = require("fs");
 server.listen(process.env.PORT || 3000, "0.0.0.0");
-console.log(req.hostname)
+console.log(req.hostname);
 console.log('Server Running Test No7');
 
 var arrayUser = [];
