9634b8ba8899feb9703cf68301dadfd732b783f4
DucTuan1802 <105335784+DucTuan1802@users.noreply.github.com>
2022-05-11 16:50:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require("express");
 var app = express();
 var server = require("http").createServer(app);
-var io = require("socket.io")(server);
+var io = require("socket.io").listen(server);
 var fs = require("fs");
 server.listen(process.env.PORT || 3000);
 
-console.log('Server Running Test');
+console.log('Server Running Test 2');
 
 var arrayUser = [];
 var existFlag = true;
